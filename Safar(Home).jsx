0c59d0cf901c926378dfd22809524548bc23ad85
Rabari Janu <163220232+RabariJanu@users.noreply.github.com>
2024-12-18 12:49:00 +0530

    Create Safar(Home).jsx

---
+++ Safar(Home).jsx
@@ -0,0 +1,26 @@
+<div className="Group14" style={{width: 1442, height: 776, position: 'relative'}}>
+  <div className="Rectangle12" style={{width: 1440, height: 776, left: 0, top: 0, position: 'absolute', background: 'rgba(74.84, 150.21, 171.06, 0.10)'}} />
+  <div className="SubscribeForFreshContectProductUpdate" style={{width: 471, height: 56, left: 70, top: 89, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'}}>SUBSCRIBE FOR FRESH<br/>CONTECT & PRODUCT UPDATE</div>
+  <div className="Group15" style={{width: 327, height: 37, left: 70, top: 157, position: 'absolute'}}>
+    <div className="Rectangle13" style={{width: 327, height: 37, left: 0, top: 0, position: 'absolute', borderRadius: 25, border: '5px rgba(0, 0, 0, 0.44) solid'}} />
+    <div className="EnterYourEmail" style={{width: 182, height: 27, left: 29, top: 5, position: 'absolute', color: 'rgba(0, 0, 0, 0.50)', fontSize: 20, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'}}>Enter Your Email</div>
+  </div>
+  <div className="Group16" style={{width: 150, height: 29, left: 70, top: 212, position: 'absolute'}}>
+    <div className="Rectangle14" style={{width: 150, height: 29, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 25, border: '1px black solid'}} />
+    <div className="Subscribe" style={{width: 141, height: 25, left: 9, top: 4, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 22, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'}}>Subscribe</div>
+  </div>
+  <div className="Group17" style={{width: 506, height: 220, left: 838, top: 89, position: 'absolute'}}>
+    <div className="InformationAboutUsBlogPackageFAQ" style={{width: 150, height: 220, left: 356, top: 0, position: 'absolute'}}><span style="color: 'black', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">Information<br/><br/></span><span style="color: 'rgba(0, 0, 0, 0.70)', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">About us<br/>Blog<br/>Package<br/>F.A.Q<br/></span><span style="color: 'black', fontSize: 22, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'"><br/></span></div>
+    <div className="ProductCarHireVisaPackagePassportInsuranceEducation" style={{width: 150, height: 220, left: 178, top: 0, position: 'absolute'}}><span style="color: 'black', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">Product<br/><br/></span><span style="color: 'rgba(0, 0, 0, 0.70)', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">Car hire<br/>Visa<br/>Package<br/>Passport<br/>Insurance<br/>Education<br/></span></div>
+    <div className="TourAsiaAfricaAmericaChinaHoliday" style={{width: 150, height: 220, left: 0, top: 0, position: 'absolute'}}><span style="color: 'black', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">Tour <br/></span><span style="color: 'rgba(0, 0, 0, 0.50)', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'"><br/></span><span style="color: 'rgba(0, 0, 0, 0.70)', fontSize: 24, fontFamily: 'Times New Roman', fontWeight: '700', wordWrap: 'break-word'">Asia<br/>Africa<br/>America<br/>China<br/>Holiday<br/></span></div>
+  </div>
+  <div className="Group18" style={{width: 1442, height: 300, left: 0, top: 409, position: 'absolute'}}>
+    <img className="P5" style={{width: 195.92, height: 250, left: 207.68, top: 49, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x250" />
+    <img className="P6" style={{width: 195.92, height: 300, left: 830.72, top: -0, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x300" />
+    <img className="P7" style={{width: 195.92, height: 250, left: 1038.40, top: 50, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x250" />
+    <img className="P4" style={{width: 195.92, height: 300, left: 415.36, top: -0, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x300" />
+    <img className="P3" style={{width: 195.92, height: 300, left: 1246.08, top: -0, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x300" />
+    <img className="P2" style={{width: 195.92, height: 250, left: 623.04, top: 50, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x250" />
+    <img className="P1" style={{width: 195.92, height: 300, left: 0, top: 0, position: 'absolute', borderRadius: 15}} src="https://via.placeholder.com/196x300" />
+  </div>
+</div>
